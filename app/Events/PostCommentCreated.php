898239f28d84f983a27902f4768a12fc654aa8b2
Wailan Tirajoh <wailan.tirajoh@concrete-tech.id>
2022-01-28 11:05:13 +0700

    change: Refactor events, remove unused class

--- app/Events/PostCommentCreated.php
+++ app/Events/PostCommentCreated.php
@@ -2,9 +2,7 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
