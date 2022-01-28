f3ce5d5d05acd6c2235e905eff7f767b8da6c502
Wailan Tirajoh <wailan.tirajoh@concrete-tech.id>
2022-01-28 11:13:29 +0700

    change: remove unused class, add message to response json, remove user variable on invoke method [RegisterController]

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -5,19 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class RegisterController extends Controller
 {
     public function __invoke(RegisterRequest $request)
     {
-        $user = User::create([
+        User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
 
-        return response()->json($user, Response::HTTP_OK);
+        return response()->json([
+            'message' => 'User created successfully, please login to continue'
+        ], Response::HTTP_OK);
     }
 }
