6f6d739e6db037aa4bea289f2813fdc67f3ab925
Wailan Tirajoh <wailantirajoh@gmail.com>
2022-02-04 01:38:00 +0700

    add: add post controller [forgot add]

---
+++ app/Http/Controllers/Api/V1/PostPinController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+
+class PostPinController extends Controller
+{
+    public function __invoke(Post $post)
+    {
+        $post->updatePin();
+
+        if ($post->is_pinned) {
+            $message = 'Post pinned successfully';
+        } else {
+            $message = 'Post unpinned successfully';
+        }
+
+        return response()->json([
+            'message' => $message,
+            'post' => PostResource::make($post)
+        ]);
+    }
+}
