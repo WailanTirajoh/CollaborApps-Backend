3dc6d39bc14e828f667c816df47aad46c7d64f77
Wailan Tirajoh <wailantirajoh@gmail.com>
2022-02-04 23:39:35 +0900

    change: migration post from string to longText

--- database/migrations/2022_01_22_092941_create_posts_table.php
+++ database/migrations/2022_01_22_092941_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->string('text');
+            $table->longText('text');
             $table->timestamps();
         });
     }
