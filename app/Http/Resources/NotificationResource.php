9a354c0b4a5a6bcd52a53d35f1bc12783ce96fd7
Wailan Tirajoh <wailan.tirajoh@kawanlamaretail.com>
2022-02-11 00:36:32 +0700

    change: notification resource

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -14,6 +14,9 @@ class NotificationResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'comment' => $this->data,
+        ];
     }
 }
